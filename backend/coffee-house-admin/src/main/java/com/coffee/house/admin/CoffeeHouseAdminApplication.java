367492c554a9f67421c7740d561eacf5ca5cb767
= <=>
2023-12-30 15:38:35 +0700

    test

--- backend/coffee-house-admin/src/main/java/com/coffee/house/admin/CoffeeHouseAdminApplication.java
+++ backend/coffee-house-admin/src/main/java/com/coffee/house/admin/CoffeeHouseAdminApplication.java
@@ -2,10 +2,8 @@ package com.coffee.house.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("com.coffee.house.models")
 public class CoffeeHouseAdminApplication {
 
 	public static void main(String[] args) {
